400f146b48f2d8ebc10d139ae28ef8de21f44438
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-04-01 14:07:20 -0700

    http://localhost:9000/api/<group id>/participants returning a list of participants

--- server/api/group/group.controller.js
+++ server/api/group/group.controller.js
@@ -3,16 +3,19 @@
 var _ = require('lodash');
 var underscore = require('underscore');
 var Group = require('./group.model');
+var User = require('../user/user.model');
 
 // Get a list of groups a given user is registered for
 exports.index = function(req, res) {
-  Group.find(function (err, groups) {
+  Group.find(function(err, groups) {
     var selectedGroups = [];
-    if(err) { return handleError(res, err); }
-    for (var i = 0 ; i < groups.length; i ++) {
+    if (err) {
+      return handleError(res, err);
+    }
+    for (var i = 0; i < groups.length; i++) {
       if (underscore.contains(groups[i].participants, req.params.uid)) {
         selectedGroups.push(groups[i]);
-      } 
+      }
     }
     return res.status(200).json(selectedGroups);
   });
@@ -20,30 +23,66 @@ exports.index = function(req, res) {
 
 // Get a single group
 exports.show = function(req, res) {
-  Group.findById(req.params.id, function (err, group) {
-    if(err) { return handleError(res, err); }
-    if(!group) { return res.status(404).send('Not Found'); }
+  Group.findById(req.params.id, function(err, group) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!group) {
+      return res.status(404).send('Not Found');
+    }
     return res.json(group);
+  });
+};
+
+// Get a list of participants given a group id
+exports.showParticipants = function(req, res) {
+  Group.findById(req.params.id, function(err, group) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!group) {
+      return res.status(404).send('Not Found');
+    }
+
+    var participantIds = group.participants;
+    User.find({
+      '_id': {
+        $in: participantIds
+      }
+    }, function(err, docs) {
+      return res.json(docs);
+    });
+
   });
 };
 
 // Creates a new group in the DB.
 exports.create = function(req, res) {
   Group.create(req.body, function(err, group) {
-    if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
     return res.status(201).json(group);
   });
 };
 
 // Updates an existing group in the DB.
 exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  Group.findById(req.params.id, function (err, group) {
-    if (err) { return handleError(res, err); }
-    if(!group) { return res.status(404).send('Not Found'); }
+  if (req.body._id) {
+    delete req.body._id;
+  }
+  Group.findById(req.params.id, function(err, group) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!group) {
+      return res.status(404).send('Not Found');
+    }
     var updated = _.merge(group, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
+    updated.save(function(err) {
+      if (err) {
+        return handleError(res, err);
+      }
       return res.status(200).json(group);
     });
   });
@@ -51,15 +90,23 @@ exports.update = function(req, res) {
 
 // add this user to the group
 exports.addUser = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  Group.findById(req.params.gid, function (err, group) {
-    if (err) { return handleError(res, err); }
-    if(!group) { return res.status(404).send('Not Found'); }
+  if (req.body._id) {
+    delete req.body._id;
+  }
+  Group.findById(req.params.gid, function(err, group) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!group) {
+      return res.status(404).send('Not Found');
+    }
     var thisUser = [];
     thisUser.push(req.params.uid);
     group.participants = underscore.union(group.participants, thisUser);
-    group.save(function (err) {
-      if (err) { return handleError(res, err); }
+    group.save(function(err) {
+      if (err) {
+        return handleError(res, err);
+      }
       return res.status(200).json(group);
     });
   });
@@ -67,11 +114,17 @@ exports.addUser = function(req, res) {
 
 // Deletes a group from the DB.
 exports.destroy = function(req, res) {
-  Group.findById(req.params.id, function (err, group) {
-    if(err) { return handleError(res, err); }
-    if(!group) { return res.status(404).send('Not Found'); }
+  Group.findById(req.params.id, function(err, group) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!group) {
+      return res.status(404).send('Not Found');
+    }
     group.remove(function(err) {
-      if(err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.status(204).send('No Content');
     });
   });
