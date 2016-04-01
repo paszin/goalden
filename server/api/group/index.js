99af15146e1a783b8e2232da3f3c39d2d06164ab
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-04-01 14:07:01 -0700

    showParticipants added to index

--- server/api/group/index.js
+++ server/api/group/index.js
@@ -7,6 +7,7 @@ var router = express.Router();
 
 router.get('/users/:uid/groups', controller.index);
 router.get('/:id', controller.show);
+router.get('/:id/participants', controller.showParticipants);
 router.get('/:gid/:uid/add', controller.addUser);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
