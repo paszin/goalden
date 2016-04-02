dcc61f23750a5e9ccbde0ceae477be1900e73f71
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-04-02 10:31:30 -0700

    showGames added

--- server/api/game/index.js
+++ server/api/game/index.js
@@ -12,5 +12,6 @@ router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
 router.delete('/:id', controller.destroy);
 router.post('/:gid/users/:uid/join', controller.addUser);
+router.get('/users/:uid', controller.showGames);
 
 module.exports = router;
