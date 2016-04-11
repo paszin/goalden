7b3cea5e7664cdebda58bcbfbb0e8c5bfccd9582
Pascal <pascalslogin@gmail.com>
2016-04-11 01:00:48 -0700

    feedback page minor changes

--- client/app/feedback/feedback.controller.js
+++ client/app/feedback/feedback.controller.js
@@ -1,17 +1,16 @@
 'use strict';
 
 angular.module('goaldenAppApp')
-    .controller('FeedbackCtrl', function ($scope, Auth) {
-        var user = Auth.getCurrentUser();
-        $scope.profile = {
-            feedback_count: user.feedback_count,
-            games_count: user.games_count
-        };
-        $scope.data = {
-            labels: ['Thumbs Up', 'Positive<br>Attitude', 'Excellent Player', 'High Five'],
-            series: [
-                    [user.feedback.thumbs_up, user.feedback.smile, user.feedback.excellent_player, user.feedback.high_five]
-                ],
-            horizontalBars: true
-        };
+    .controller('FeedbackCtrl', function ($scope, User) {
+        User.get(function (user) {
+            $scope.profile = user;
+            $scope.data = {
+                labels: ['Thumbs Up', 'Positive<br>Attitude', 'Excellent Player', 'High Five']
+                , series: [
+                    [user.feedback.thumbs_up, user.feedback.smile, user.feedback.excellent_player, $scope.profile.feedback.high_five]
+                ]
+                , horizontalBars: true
+            };
+        });
+
     });
