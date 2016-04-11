026a9caf99a656e223a5c8350297044c4345c9c1
Pascal <pascalslogin@gmail.com>
2016-04-11 01:29:19 -0700

    fixed feedback page and reduced to only the chart

--- client/app/feedback/feedback.controller.js
+++ client/app/feedback/feedback.controller.js
@@ -2,15 +2,25 @@
 
 angular.module('goaldenAppApp')
     .controller('FeedbackCtrl', function ($scope, User) {
-        User.get(function (user) {
-            $scope.profile = user;
-            $scope.data = {
-                labels: ['Thumbs Up', 'Positive<br>Attitude', 'Excellent Player', 'High Five']
-                , series: [
-                    [user.feedback.thumbs_up, user.feedback.smile, user.feedback.excellent_player, $scope.profile.feedback.high_five]
+
+        $scope.refresh = function () {
+            User.get(function (user) {
+                var feedback = {};
+                for (var i=0; i<user.feedback.length; i++) {
+                    feedback[user.feedback[i].name] = user.feedback[i].count; 
+                }
+                $scope.profile = user;
+                $scope.data = {
+                    labels: ['Thumbs Up', 'Positive<br>Attitude', 'Excellent Player', 'High Five']
+                    , series: [
+                    [feedback.thumbs_up, feedback.smile, feedback.excellent_player, feedback.high_five]
                 ]
-                , horizontalBars: true
-            };
-        });
+                    , horizontalBars: true
+                };
+            });
+
+        };
+    
+    $scope.refresh();
 
     });
