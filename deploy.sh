46f07606c4699ba99bd2aaa9a9789c194fdf0716
Pascal <pascalslogin@gmail.com>
2016-04-12 12:39:51 -0700

    deployment as shell script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+git fetch origin master
+git reset --hard FETCH_HEAD
+git clean -df
+export NODE_ENV="development"
+npm install
+bower install
+grunt build
+export NODE_ENV="production"
+export PORT=9000
+node dist/server/app.js
