7c116f015bf4419e593c6bee971e2cbcccf6c4ca
Pascal <pascalslogin@gmail.com>
2016-04-14 01:05:30 -0700

    removed groups from frontend

--- client/app/groups/groups.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('goaldenAppApp')
-  .controller('GroupsCtrl', function ($scope, Auth) {
-    $scope.aNumber = Math.round(Math.random()*8);
-    $scope.user = Auth.getCurrentUser();
-  });
